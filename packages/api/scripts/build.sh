99b5bf65d82e8625c9fb7220e155ea8c94e34529
Bilyal Mestanov <bmestanov@gmail.com>
2023-12-01 15:56:58 +0100

    feat: add dockerfile

--- packages/api/scripts/build.sh
+++
@@ -1,4 +0,0 @@
-yarn prisma:generate
-yarn build
-
-echo "$TLS_CA_CERT_MONGO" >$PWD/ca-mongo.crt
--- packages/api/scripts/start.sh
+++ packages/api/scripts/start.sh
@@ -1,4 +1,3 @@
-DATABASE_URL=$DATABASE_URL_NON_POOLED
-yarn prisma:migrate
+DATABASE_URL=$DATABASE_URL_NON_POOLED yarn prisma:migrate
 
 node ../dist/server.js
