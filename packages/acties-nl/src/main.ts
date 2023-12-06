2418bcc31ac161541e1b4cd2010a058f70d84552
adnaydenov <github@anaydenov.com>
2023-12-06 14:29:28 +0200

    Adding acties-nl actor

---
+++ packages/acties-nl/src/main.ts
@@ -0,0 +1,68 @@
+// For more information, see https://crawlee.dev/
+import { Actor } from 'apify';
+import { CheerioCrawler } from 'crawlee';
+import { Label, sitemapHandler, listingHandler, codeHandler } from './routes';
+
+const startUrl = 'https://www.acties.nl/sitemap.xml';
+
+// Define the main run function as an async function
+async function main() {
+  await Actor.init();
+
+  // Initialize the request queue
+  const requestQueue = await Actor.openRequestQueue();
+
+  // use proxy configuration if provided in input
+  const input: any = await Actor.getInput();
+  const proxyConfiguration = await Actor.createProxyConfiguration(
+    input?.proxyConfiguration
+  );
+
+  let effectiveTestLimit = 0;
+  if (typeof input?.testLimit === 'number' && input?.testLimit > 0) {
+    effectiveTestLimit = input?.testLimit;
+  }
+
+  const crawler = new CheerioCrawler({
+    proxyConfiguration, // Use this if you need proxy configuration, else comment it out or remove
+    requestQueue,
+    requestHandler: async (context) => {
+      switch (context.request.userData.label) {
+        case Label.sitemap:
+          await sitemapHandler(requestQueue, context);
+          break;
+        case Label.listing:
+          await listingHandler(requestQueue, context);
+          break;
+        case Label.getCode:
+          await codeHandler(requestQueue, context);
+          break;
+        default:
+          throw new Error('Unknown label');
+      }
+    },
+    // Additional options can go here, e.g., maxConcurrency, requestTimeouts, etc.
+  });
+
+  // Adding the initial request with a handlerLabel in userData
+  await crawler.addRequests([
+    {
+      url: startUrl,
+      label: Label.sitemap,
+      userData: {
+        testLimit: effectiveTestLimit,
+      },
+    },
+  ]);
+
+  // Run the crawler with the startUrls array
+  await crawler.run();
+
+  await Actor.exit();
+}
+
+// Call the main function and properly handle any errors
+main().catch((error) => {
+  console.error('Error occurred:', error);
+  process.exit(1);
+});
