5547d2c4e48ce5c40f8ae3992d23a793404949c5
adnaydenov <github@anaydenov.com>
2024-02-24 14:52:16 +0200

    refactor(lovecoupons-com-sg): Implementing /match-ids API check for existing coupons

--- packages/lovecoupons-com-sg/src/routes.ts
+++ packages/lovecoupons-com-sg/src/routes.ts
@@ -1,9 +1,16 @@
 import cheerio from 'cheerio';
-import { RequestProvider } from 'crawlee';
 import { createCheerioRouter } from 'crawlee';
 import * as he from 'he';
 import { DataValidator } from 'shared/data-validator';
-import { processAndStoreData, sleep, getDomainName } from 'shared/helpers';
+import {
+  processAndStoreData,
+  sleep,
+  getDomainName,
+  generateCouponId,
+  checkCouponIds,
+  CouponItemResult,
+  CouponHashMap,
+} from 'shared/helpers';
 import { Label, CUSTOM_HEADERS } from 'shared/actor-utils';
 
 function extractAllText(elem: cheerio.Cheerio): string {
@@ -21,14 +28,13 @@ function extractAllText(elem: cheerio.Cheerio): string {
   return text.trim();
 }
 
-async function processCouponItem(
-  requestQueue: RequestProvider,
+function processCouponItem(
   merchantName: string,
   domain: string,
   isExpired: boolean,
   couponElement: cheerio.Element,
   sourceUrl: string
-) {
+): CouponItemResult {
   const $coupon = cheerio.load(couponElement);
 
   const attrDataType = $coupon('*').first().attr('data-type');
@@ -97,22 +103,9 @@ async function processCouponItem(
   validator.addValue('isExpired', isExpired);
   validator.addValue('isShown', true);
 
-  if (hasCode) {
-    // Add the coupon URL to the request queue
-    await requestQueue.addRequest(
-      {
-        url: couponUrl,
-        userData: {
-          label: Label.getCode,
-          validatorData: validator.getData(),
-        },
-        headers: CUSTOM_HEADERS,
-      },
-      { forefront: true }
-    );
-  } else {
-    await processAndStoreData(validator);
-  }
+  const generatedHash = generateCouponId(merchantName, idInSite, sourceUrl);
+
+  return { generatedHash, hasCode, couponUrl, validator };
 }
 
 export const router = createCheerioRouter();
@@ -152,17 +145,46 @@ router.addHandler(Label.listing, async (context) => {
     // console.log(`Merchant Domain: ${domain}`);
 
     // Extract valid coupons
+    const couponsWithCode: CouponHashMap = {};
+    const idsToCheck: string[] = [];
+    let result: CouponItemResult;
     const validCoupons = $('div.BrandOffers > article');
     for (let i = 0; i < validCoupons.length; i++) {
       const element = validCoupons[i];
-      await processCouponItem(
-        crawler.requestQueue,
+      result = processCouponItem(
         merchantName,
         domain,
         false,
         element,
         request.url
       );
+      if (!result.hasCode) {
+        await processAndStoreData(result.validator);
+      } else {
+        couponsWithCode[result.generatedHash] = result;
+        idsToCheck.push(result.generatedHash);
+      }
+    }
+    // Call the API to check if the coupon exists
+    const nonExistingIds = await checkCouponIds(idsToCheck);
+
+    if (nonExistingIds.length > 0) {
+      let currentResult: CouponItemResult;
+      for (const id of nonExistingIds) {
+        currentResult = couponsWithCode[id];
+        // Add the coupon URL to the request queue
+        await crawler.requestQueue.addRequest(
+          {
+            url: currentResult.couponUrl,
+            userData: {
+              label: Label.getCode,
+              validatorData: currentResult.validator.getData(),
+            },
+            headers: CUSTOM_HEADERS,
+          },
+          { forefront: true }
+        );
+      }
     }
   } catch (error) {
     console.error(
