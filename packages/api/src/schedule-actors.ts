ffdf616bae4001d3728ff4617cd941b2d5a62464
Emil Kirilov <emil.kirilovk@gmail.com>
2024-03-14 11:50:59 +0200

    Fix env variables discovery in schedule-actor.ts

--- packages/api/src/schedule-actors.ts
+++ packages/api/src/schedule-actors.ts
@@ -1,11 +1,3 @@
-import dotenv from 'dotenv';
-
-const parseData = dotenv.config({ path: '/app/packages/api/.env' });
-
-const envData = parseData.parsed || undefined;
-
-if (envData === undefined) throw new Error('The .env file could not be parsed');
-
 const MAX_CONCURRENT_RUNS = 2;
 const FINISHED_STATUSES = new Set([
   'SUCCEEDED',
@@ -13,7 +5,7 @@ const FINISHED_STATUSES = new Set([
   'ABORTED',
   'TIMED_OUT',
 ]);
-const APIFY_GET_ALL_RUNS_URL = `https://api.apify.com/v2/actor-runs?token=${envData.API_KEY_APIFY}&desc=true`;
+const APIFY_GET_ALL_RUNS_URL = `https://api.apify.com/v2/actor-runs?token=${process.env.API_KEY_APIFY}&desc=true`;
 
 const findActors = async () => {
   const response = await fetch(APIFY_GET_ALL_RUNS_URL, {
@@ -31,11 +23,11 @@ const findActors = async () => {
 
   if (maxConcurrency < 1) return;
 
-  fetch(`${envData.BASE_URL}/targets/run`, {
+  fetch(`${process.env.BASE_URL}/targets/run`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: envData.API_SECRET as string,
+      Authorization: process.env.API_SECRET as string,
     },
     body: JSON.stringify({ maxConcurrency }),
   });
