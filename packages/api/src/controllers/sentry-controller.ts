6a70a12270cabb42a71918a8710b15ad11f2c648
adnaydenov <github@anaydenov.com>
2024-03-22 00:18:12 +0200

    Update Sentry DSN in SentryController

--- packages/api/src/controllers/sentry-controller.ts
+++ packages/api/src/controllers/sentry-controller.ts
@@ -1,13 +1,12 @@
 import { JsonController, Get } from 'routing-controllers';
 
-const SENTRY_DSN="https://87899ea721020ea71b712cb6e26b5861@o4506914093531136.ingest.us.sentry.io/4506930859278336"
-
 @JsonController('/sentry')
 export class SentryController {
   @Get('/dsn')
   getSentryUrl() {
     // const sentryUrl = process.env.SENTRY_DSN;
-    const sentryUrl = SENTRY_DSN;
+    const sentryUrl =
+      'https://87899ea721020ea71b712cb6e26b5861@o4506914093531136.ingest.us.sentry.io/4506930859278336';
     if (!sentryUrl) {
       return {
         error: 'Sentry URL is not configured.',
