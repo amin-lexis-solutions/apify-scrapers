7c11fe37d6f91938fc00505b4f9c43633999eeed
Ronaldo Mata <ronaldomata34@gmail.com>
2024-03-25 18:03:12 -0400

    gutscheinsammler-de actor added

---
+++ packages/gutscheinsammler-de/src/main.ts
@@ -0,0 +1,17 @@
+import 'shared/sentry-init';
+import { Actor } from 'apify';
+import { prepareCheerioScraper } from 'shared/actor-utils';
+import { router } from './routes';
+
+async function main() {
+  await Actor.init();
+
+  const crawler = await prepareCheerioScraper(router, {
+    customHeaders: { Origin: 'https://www.picodi.com' },
+  });
+
+  await crawler.run();
+  await Actor.exit();
+}
+
+main();
