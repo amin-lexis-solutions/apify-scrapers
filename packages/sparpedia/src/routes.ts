bc718583d9adad12f92a8815018062d95d73a7a3
Ronaldo Mata <ronaldomata34@gmail.com>
2024-03-28 19:26:05 -0400

    sparpedia actor added

---
+++ packages/sparpedia/src/routes.ts
@@ -0,0 +1,219 @@
+import cheerio from 'cheerio';
+import * as he from 'he';
+import { createCheerioRouter } from 'crawlee';
+import { DataValidator } from 'shared/data-validator';
+import {
+  processAndStoreData,
+  sleep,
+  generateCouponId,
+  checkCouponIds,
+  CouponItemResult,
+  CouponHashMap,
+} from 'shared/helpers';
+import { Label, CUSTOM_HEADERS } from 'shared/actor-utils';
+
+function processCouponItem(
+  merchantName: string,
+  isExpired: boolean,
+  couponElement: cheerio.Element,
+  sourceUrl: string
+): CouponItemResult {
+  const $coupon = cheerio.load(couponElement);
+
+  const idAttr = $coupon('*').first().attr('id')?.trim();
+  if (!idAttr) {
+    console.log('Coupon HTML:', $coupon.html());
+    throw new Error('Element ID attr is missing');
+  }
+
+  // Extract the ID from the ID attribute
+  const idInSite = idAttr.split('-').pop();
+
+  if (!idInSite) {
+    console.log('Coupon HTML:', $coupon.html());
+    throw new Error('ID in site is missing');
+  }
+
+  let hasCode = false;
+  let couponUrl = '';
+  const elementClass = $coupon('*').first().attr('class');
+  if (!elementClass) {
+    console.log('Coupon HTML:', $coupon.html());
+    throw new Error('Element class is missing');
+  }
+
+  hasCode = elementClass.includes('offer-label-type-code');
+
+  if (hasCode) {
+    couponUrl = `${sourceUrl}?popup_id=${idInSite}`;
+  }
+
+  // Extract the voucher title
+  const titleElement = $coupon('h3 > a').first();
+  if (titleElement.length === 0) {
+    console.log('Coupon HTML:', $coupon.html());
+    throw new Error('Voucher title is missing');
+  }
+  const voucherTitle = titleElement.text().trim();
+
+  // Extract the description
+  const descElement = $coupon('div.voor');
+  let description = '';
+  if (descElement.length > 0) {
+    description = descElement.text().trim();
+  }
+
+  // Check if the coupon is exclusive
+  let isExclusive = false;
+  const exclusiveElement = $coupon('span.label-exclusive');
+  if (exclusiveElement.length > 0) {
+    isExclusive = true;
+  }
+
+  const validator = new DataValidator();
+
+  // Add required and optional values to the validator
+  validator.addValue('sourceUrl', sourceUrl);
+  validator.addValue('merchantName', merchantName);
+  validator.addValue('title', voucherTitle);
+  validator.addValue('idInSite', idInSite);
+  validator.addValue('description', description);
+  validator.addValue('isExpired', isExpired);
+  validator.addValue('isExclusive', isExclusive);
+  validator.addValue('isShown', true);
+
+  const generatedHash = generateCouponId(merchantName, idInSite, sourceUrl);
+
+  return { generatedHash, hasCode, couponUrl, validator };
+}
+
+export const router = createCheerioRouter();
+
+router.addHandler(Label.listing, async (context) => {
+  const { request, $, crawler } = context;
+
+  if (request.userData.label !== Label.listing) return;
+
+  if (!crawler.requestQueue) {
+    throw new Error('Request queue is missing');
+  }
+
+  try {
+    // Extracting request and body from context
+
+    console.log(`\nProcessing URL: ${request.url}`);
+
+    const merchantElem = $('ol.mrk-breadcrumbs > li:last-child');
+
+    const merchantName = he.decode(
+      merchantElem ? merchantElem.text().trim() : ''
+    );
+
+    if (!merchantName) {
+      throw new Error('Merchant name is missing');
+    }
+
+    const couponsWithCode: CouponHashMap = {};
+    const idsToCheck: string[] = [];
+    let result: CouponItemResult;
+
+    // Extract valid coupons
+    const validCoupons = $(
+      'div.current-shop-offers > div.offer-default.not-expired'
+    );
+    for (let i = 0; i < validCoupons.length; i++) {
+      const element = validCoupons[i];
+      result = processCouponItem(merchantName, false, element, request.url);
+      if (!result.hasCode) {
+        await processAndStoreData(result.validator);
+      } else {
+        couponsWithCode[result.generatedHash] = result;
+        idsToCheck.push(result.generatedHash);
+      }
+    }
+
+    // Extract expired coupons
+    const expiredCoupons = $(
+      'div.current-shop-offers > div.offer-default.has-expired'
+    );
+    for (let i = 0; i < expiredCoupons.length; i++) {
+      const element = expiredCoupons[i];
+      result = processCouponItem(merchantName, true, element, request.url);
+      if (!result.hasCode) {
+        await processAndStoreData(result.validator);
+      } else {
+        couponsWithCode[result.generatedHash] = result;
+        idsToCheck.push(result.generatedHash);
+      }
+    }
+
+    // Call the API to check if the coupon exists
+    const nonExistingIds = await checkCouponIds(idsToCheck);
+
+    if (nonExistingIds.length <= 0) {
+      return;
+    }
+
+    let currentResult: CouponItemResult;
+    for (const id of nonExistingIds) {
+      currentResult = couponsWithCode[id];
+      // Add the coupon URL to the request queue
+      await crawler.requestQueue.addRequest(
+        {
+          url: currentResult.couponUrl,
+          userData: {
+            label: Label.getCode,
+            validatorData: currentResult.validator.getData(),
+          },
+          headers: CUSTOM_HEADERS,
+        },
+        { forefront: true }
+      );
+    }
+  } finally {
+    // We don't catch so that the error is logged in Sentry, but use finally
+    // since we want the Apify actor to end successfully and not waste resources by retrying.
+  }
+});
+
+router.addHandler(Label.getCode, async (context) => {
+  // context includes request, body, etc.
+  const { request, $ } = context;
+
+  if (request.userData.label !== Label.getCode) return;
+
+  try {
+    // Sleep for x seconds between requests to avoid rate limitings
+    await sleep(1000);
+
+    // Retrieve validatorData from request's userData
+    const validatorData = request.userData.validatorData;
+
+    // Create a new DataValidator instance and load the data
+    const validator = new DataValidator();
+    validator.loadData(validatorData);
+
+    const codeInput = $('div.code-popup > input[type="text"]').first();
+    if (!codeInput) {
+      throw new Error('Coupon code input element is missing');
+    }
+    const code = codeInput.val()?.trim();
+
+    // Check if the code is found
+    if (!code) {
+      console.log('Coupon HTML:', $.html());
+      throw new Error('Coupon code not found in the HTML content');
+    }
+
+    console.log(`Found code: ${code}\n    at: ${request.url}`);
+
+    // Add the decoded code to the validator's data
+    validator.addValue('code', code);
+
+    // Process and store the data
+    await processAndStoreData(validator);
+  } finally {
+    // We don't catch so that the error is logged in Sentry, but use finally
+    // since we want the Apify actor to end successfully and not waste resources by retrying.
+  }
+});
