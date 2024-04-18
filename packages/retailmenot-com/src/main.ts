3d1b8c057c326ff00070fcc1c77f8870b4302ca6
adnaydenov <github@anaydenov.com>
2024-04-18 18:48:26 +0300

    Add new package retailmenot-com with main.ts, README.md, tsconfig.json, and package.json

---
+++ packages/retailmenot-com/src/main.ts
@@ -0,0 +1,16 @@
+import 'shared/sentry-init';
+import { Actor } from 'apify';
+import { preparePuppeteerScraper } from 'shared/actor-utils';
+
+import { router } from './routes';
+
+async function main() {
+  await Actor.init();
+
+  const crawler = await preparePuppeteerScraper(router as any, {});
+
+  await crawler.run();
+  await Actor.exit();
+}
+
+main();
