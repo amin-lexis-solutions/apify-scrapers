fb5bea2f14bf932b2a24ba94047d5895d6d3a1e6
RM02 <rafamark244@gmail.com>
2024-04-23 16:53:48 -0400

    lint:fix

--- packages/couponzguru-sg/src/routes.ts
+++ packages/couponzguru-sg/src/routes.ts
@@ -18,7 +18,6 @@ function processCouponItem(
   element: cheerio.Element,
   sourceUrl: string
 ): CouponItemResult {
-  
   // Load the coupon element using Cheerio
   const $coupon = cheerio.load(element);
   // Extract data
@@ -29,7 +28,7 @@ function processCouponItem(
     .replaceAll('\n', ' ')
     ?.trim();
   const idInSite = $coupon('.hide').prev().attr('id')?.split('hide-')?.[1];
-  
+
   // Throw an error if ID is not found
   if (!idInSite) {
     throw new Error('Element data-promotion-id attr is missing');
@@ -46,12 +45,12 @@ function processCouponItem(
   validator.addValue('description', desc);
   validator.addValue('isExpired', false);
   validator.addValue('isShown', hasCode);
-  
+
   // If coupon code exists, set hasCode to true and add code to validator
   if (hasCode) {
     validator.addValue('code', code);
   }
-  
+
   // Generate a hash for the coupon
   const generatedHash = generateCouponId(merchantName, idInSite, sourceUrl);
   // Return the coupon item result
@@ -59,12 +58,11 @@ function processCouponItem(
 }
 // Handler function for processing coupon listings
 router.addHandler(Label.listing, async ({ request, $, log }) => {
-  
   try {
     log.info(`Listing ${request.url}`);
 
     const merchantName = $('.brand-heading h1').text()?.split(' ')?.[0];
-    
+
     // Throw an error if merchant name is not found
     if (!merchantName) {
       throw new Error('merchantName not found');
@@ -75,7 +73,7 @@ router.addHandler(Label.listing, async ({ request, $, log }) => {
     const couponsWithCode: CouponHashMap = {};
     const idsToCheck: string[] = [];
     let result: CouponItemResult;
-    
+
     // Loop through each coupon element and process it
     for (const coupon of validCoupons) {
       result = processCouponItem(merchantName, coupon, request.url);
@@ -87,7 +85,7 @@ router.addHandler(Label.listing, async ({ request, $, log }) => {
         await processAndStoreData(result.validator);
       }
     }
-    
+
     // Call the API to check if the coupon exists
     const nonExistingIds = await checkCouponIds(idsToCheck);
     // If non-existing coupons are found, process and store their data
