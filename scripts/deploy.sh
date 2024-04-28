46ed1bf9239e2fc2eb852cae421f6a850565e2d5
Amin Mbarki <amin@lexis.solutions>
2024-04-28 18:39:06 +0100

    Implement CI Action for Apify Actor deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the list of packages that have been modified in the current branch
+packages=$(git diff --name-only origin/ci-deploy-actor packages/ | grep -v 'packages/api\|packages/shared' | cut -d/ -f2 | cut -d/ -f1 | uniq)
+
+# Loop through the list of folders and deploy each package
+for package in $packages
+do
+    # Check if the package has a ./src/main.ts file
+    if [ -f packages/$package/src/main.ts ]; then
+        # set the TYPE to "puppeteer" as default
+        TYPE="puppeteer"
+        # check if the file contain "prepareCheerioScraper" the update TYPE="cheerio" 
+        if grep -q "prepareCheerioScraper" packages/$package/src/main.ts; then
+            TYPE="cheerio"
+        fi
+        # the run this deploy-apify.js script with the package name and the type
+        node scripts/deploy-apify.js $package $TYPE
+        
+    else
+        # if the package does not have a ./src/main.ts file, skip the deployment
+        echo "Skipping deployment of $package"
+    fi
+done
+
