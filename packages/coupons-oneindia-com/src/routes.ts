3690668a5bb4ec1df9342686b9db2ef10acd5175
betacaroteno <rafael@lexis.solutions>
2024-05-05 11:16:40 -0400

    lint:fix

--- packages/coupons-oneindia-com/src/routes.ts
+++ packages/coupons-oneindia-com/src/routes.ts
@@ -54,90 +54,87 @@ function processCouponItem(
   return { generatedHash, hasCode, couponUrl, validator };
 }
 
-router.addHandler(
-  Label.listing,
-  async ({ request, page, log, enqueueLinks }) => {
-    if (request.userData.label !== Label.listing) return;
+router.addHandler(Label.listing, async ({ request, page, enqueueLinks }) => {
+  if (request.userData.label !== Label.listing) return;
 
-    try {
-      const jsonContent = await page.$eval(
-        'script[id="__NEXT_DATA__"]',
-        (script) => script?.textContent
-      );
+  try {
+    const jsonContent = await page.$eval(
+      'script[id="__NEXT_DATA__"]',
+      (script) => script?.textContent
+    );
 
-      let jsonData = JSON.parse(jsonContent || '{}');
-      let retailerId;
+    let jsonData = JSON.parse(jsonContent || '{}');
+    let retailerId;
 
-      if (jsonData && jsonData.props) {
-        retailerId = jsonData.query.clientId;
-        jsonData = jsonData.props.pageProps;
-      } else {
-        throw new Error('Missing jsonContent');
-      }
-      // Declarations outside the loop
-      const merchantName = jsonData.retailer.name;
-      const merchantUrl = jsonData.retailer.merchant_url;
-      const domain = getDomainName(merchantUrl);
-      // Combine active and expired vouchers
-      const activeVouchers = jsonData.vouchers.map((voucher) => ({
-        ...voucher,
-        is_expired: false,
-      }));
-
-      const expiredVouchers = jsonData.expiredVouchers.map((voucher) => ({
-        ...voucher,
-        is_expired: true,
-      }));
-
-      const vouchers = [...activeVouchers, ...expiredVouchers];
-
-      const couponsWithCode: CouponHashMap = {};
-      const idsToCheck: string[] = [];
-      let result: CouponItemResult;
-
-      for (const voucher of vouchers) {
-        result = processCouponItem(
-          merchantName,
-          domain,
-          retailerId,
-          voucher,
-          request.url
-        );
-        console.log(result.hasCode, result.couponUrl);
-        if (result.hasCode) {
-          couponsWithCode[result.generatedHash] = result;
-          idsToCheck.push(result.generatedHash);
-        } else {
-          await processAndStoreData(result.validator);
-        }
-        // Call the API to check if the coupon exists
-        const nonExistingIds = await checkCouponIds(idsToCheck);
-
-        if (nonExistingIds.length == 0) return;
-
-        let currentResult: CouponItemResult;
-
-        for (const id of nonExistingIds) {
-          currentResult = couponsWithCode[id];
-          // Add the coupon URL to the request queue
-          await enqueueLinks({
-            urls: [currentResult.couponUrl],
-            userData: {
-              label: Label.getCode,
-              validatorData: currentResult.validator.getData(),
-            },
-          });
-        }
-      }
-    } finally {
-      // We don't catch so that the error is logged in Sentry, but use finally
-      // since we want the Apify actor to end successfully and not waste resources by retrying.
+    if (jsonData && jsonData.props) {
+      retailerId = jsonData.query.clientId;
+      jsonData = jsonData.props.pageProps;
+    } else {
+      throw new Error('Missing jsonContent');
     }
+    // Declarations outside the loop
+    const merchantName = jsonData.retailer.name;
+    const merchantUrl = jsonData.retailer.merchant_url;
+    const domain = getDomainName(merchantUrl);
+    // Combine active and expired vouchers
+    const activeVouchers = jsonData.vouchers.map((voucher) => ({
+      ...voucher,
+      is_expired: false,
+    }));
+
+    const expiredVouchers = jsonData.expiredVouchers.map((voucher) => ({
+      ...voucher,
+      is_expired: true,
+    }));
+
+    const vouchers = [...activeVouchers, ...expiredVouchers];
+
+    const couponsWithCode: CouponHashMap = {};
+    const idsToCheck: string[] = [];
+    let result: CouponItemResult;
+
+    for (const voucher of vouchers) {
+      result = processCouponItem(
+        merchantName,
+        domain,
+        retailerId,
+        voucher,
+        request.url
+      );
+      console.log(result.hasCode, result.couponUrl);
+      if (result.hasCode) {
+        couponsWithCode[result.generatedHash] = result;
+        idsToCheck.push(result.generatedHash);
+      } else {
+        await processAndStoreData(result.validator);
+      }
+      // Call the API to check if the coupon exists
+      const nonExistingIds = await checkCouponIds(idsToCheck);
+
+      if (nonExistingIds.length == 0) return;
+
+      let currentResult: CouponItemResult;
+
+      for (const id of nonExistingIds) {
+        currentResult = couponsWithCode[id];
+        // Add the coupon URL to the request queue
+        await enqueueLinks({
+          urls: [currentResult.couponUrl],
+          userData: {
+            label: Label.getCode,
+            validatorData: currentResult.validator.getData(),
+          },
+        });
+      }
+    }
+  } finally {
+    // We don't catch so that the error is logged in Sentry, but use finally
+    // since we want the Apify actor to end successfully and not waste resources by retrying.
   }
-);
+});
 router.addHandler(Label.getCode, async (context) => {
   // Destructure objects from the context
-  const { request, page, log, body } = context;
+  const { request, page, log } = context;
 
   try {
     log.info(`GetCode ${request.url}`);
