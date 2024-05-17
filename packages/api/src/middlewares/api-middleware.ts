d05a552a11433000adcf54b0dd59cf269c3a1a86
Amin Mbarki <amin@lexis.solutions>
2024-05-17 10:41:03 +0100

    Add Sentry Event

--- packages/api/src/middlewares/api-middleware.ts
+++ packages/api/src/middlewares/api-middleware.ts
@@ -1,6 +1,7 @@
 import { prisma } from '../lib/prisma'; // Adjust the path according to your project structure
 import dayjs from 'dayjs';
 import { StandardResponse } from '../utils/validators';
+import * as Sentry from '@sentry/node';
 
 // CostLimit decorator to limit the cost of the API calls
 export function CostLimit() {
@@ -34,6 +35,12 @@ export function CostLimit() {
       const costLimitInUsd = Number(costLimit);
 
       if (totalCostInUsd > costLimitInUsd) {
+        Sentry.captureMessage(
+          `Cost limit reached for today. Total cost: ${totalCostInUsd.toFixed(
+            4
+          )} USD`
+        );
+
         return new StandardResponse(
           `Cost limit reached for today. Total cost: ${totalCostInUsd.toFixed(
             4
