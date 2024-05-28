d3d0d240e73598e485f74c2244e1915b2b17bb0c
rafael <rafael@lexis.solutions>
2024-05-28 09:49:00 -0400

    fix lint

--- packages/shared/src/hooks.ts
+++ packages/shared/src/hooks.ts
@@ -1,8 +1,6 @@
 import * as Sentry from '@sentry/node';
 import { checkExistingCouponsAnomaly, processAndStoreData } from './helpers';
 
-
-
 /**
  * Type definition for the AnomalyCheckHandler object.
  * @typedef {Object} AnomalyCheckHandler
