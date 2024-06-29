a4ba6555be8b2def299434402a1a4b466e075aab
Rafael Marcano <167449927+rafael-lexis-solutions@users.noreply.github.com>
2024-06-29 03:00:28 -0400

    Pelando - handle incorrect target pages (#283)
    Refactor palendo actor

--- packages/pelando-com-br/src/main.ts
+++ packages/pelando-com-br/src/main.ts
@@ -6,7 +6,10 @@ import { router } from './routes';
 async function main() {
   await Actor.init();
 
-  const crawler = await prepareCheerioScraper(router);
+  const crawler = await prepareCheerioScraper(router, {
+    indexPageSelectors: ['#store-coupons', 'ul.sc-a8fe2b69-0'],
+    nonIndexPageSelectors: ['.sc-a4b5c454-0', '#feed-tabs-container'],
+  });
 
   await crawler.run();
   await Actor.exit();
