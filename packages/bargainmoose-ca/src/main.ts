9fdd267008ead1f79216e37bb3d473748e3e4515
Amin Mbarki <amin@lexis.solutions>
2024-07-13 10:37:49 +0100

    redeploy Actor

--- packages/bargainmoose-ca/src/main.ts
+++ packages/bargainmoose-ca/src/main.ts
@@ -6,6 +6,7 @@ import { router } from './routes';
 async function main() {
   await Actor.init();
 
+  // Prepare the actor and run it
   const crawler = await prepareCheerioScraper(router, {
     indexPageSelectors: ['.promotion-filter', '.promotion-list__promotions'],
     nonIndexPageSelectors: ['.merchant-list-header-container'],
