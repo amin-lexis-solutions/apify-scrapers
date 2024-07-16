2767f77e8155d401388fa3d43f06b2df35b46026
Amin Mbarki <amin@lexis.solutions>
2024-07-16 22:57:50 +0100

    add locales seed to start.sh

--- packages/api/start.sh
+++ packages/api/start.sh
@@ -16,6 +16,11 @@ if ! DATABASE_URL=$DATABASE_URL_NON_POOLED yarn prisma:migrate; then
     echo "Failed to migrate the database."
     exit 1
 fi
+# Seed database ensuring the operation is idempotent
+if ! DATABASE_URL=$DATABASE_URL_NON_POOLED yarn prisma:seed:target-locales; then
+    echo "Failed to seed the database."
+    exit 1
+fi
 
 # Seed database ensuring the operation is idempotent
 if ! DATABASE_URL=$DATABASE_URL_NON_POOLED yarn prisma:seed:migrate-domains; then
