5e4c1900444b13a52f6af5e4ee8d1330cb83b1a9
Emil Kirilov <emil.kirilovk@gmail.com>
2024-07-24 11:29:04 +0300

    Fix domain for i-reductions.ch

--- packages/api/config/actors/i-reductions-ch.ts
+++ packages/api/config/actors/i-reductions-ch.ts
@@ -5,7 +5,7 @@ export default [
     apifyActorId: 'JJ8V36nS2Mj3YciFh',
     domains: [
       {
-        domain: 'www.i-reductions.ch',
+        domain: 'i-reductions.ch',
         locales: [Locale.fr_CH],
       },
     ],
